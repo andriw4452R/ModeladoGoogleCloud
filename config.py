90d33f9d60f8ffc281aa4db90f60b26618ca3f09
andriw4452R <Andriw_YT@outlook.com>
2025-06-05 17:15:52 -0500

    otro1

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-# Clase de configuración para la aplicación Flask
 class Config:
-    # URI de conexión a la base de datos PostgreSQL
-    # Formato: 'postgresql://usuario:contraseña@host:puerto/nombre_base_datos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Andriw4452@localhost:5432/tareasdb'
-
-    # Desactiva el seguimiento de modificaciones de objetos por parte de SQLAlchemy
-    # Esto mejora el rendimiento y evita advertencias innecesarias
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///tareas.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
