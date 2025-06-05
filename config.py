e61b7c2eb4bcc9f5daabfa9bf6b3c16d3379979e
andriw4452R <Andriw_YT@outlook.com>
2025-06-05 18:14:36 -0500

    ultimo2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tareas.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Andriw4452!@34.55.18.159:5432/tareasdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
