c67fb58d393b0084f3543ff52de3e47f4c0b811f
Jordyfr2004 <jordyalex32@gmail.com>
2025-06-13 11:58:52 -0500

    the neww

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+#configuracion y credenciales de la base de datos 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Andriw4452%21@34.55.18.159:5432/tareasdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
